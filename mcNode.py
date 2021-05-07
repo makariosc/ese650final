ec1bfb4acb9944c1dd24436e43aa521a6d11029e
Enrikina <enrikina@seas.upenn.edu>
2021-05-06 20:42:43 -0400

    (Hopefully) Finished last functions for MCST

---
+++ mcNode.py
@@ -0,0 +1,87 @@
+import collections
+import numpy as np
+import abc
+
+from utils import *
+from copy import deepcopy
+
+class MCNode(abc.ABC):
+
+    #Initializes the node.
+    def __init__(self, state):
+
+        #Saves the board state.
+        self.state = state
+
+        self.pActs = np.array([])
+
+        #Saves the node's parent and the action the parent took to reach this node. Useful for backpropogation.
+        # self.pAct = pAct
+        # self.parent = parent
+
+        #Initializes a default dictionary to save the values of "edges" (i.e. state/action pairs) important to this node.
+        self.actionDict = collections.Counter()
+
+        # Dictionary mapping move indices (see utils moveToIdx) to Children nodes and action probabilities.
+        self.children = {}
+
+        # Something like this. Runs some sort of list of possible child states on node initialization.
+        self.PotentialChildren = list(self.state.legal_moves())
+
+
+    def createChildren(self, pActs):
+        self.pActs = pActs
+        pActIdxs = np.where(pActs)
+        for moveIndex in pActIdxs:
+            move = idxToMove(moveIndex)
+            self.children[moveIndex] = Action(MCNode(deepcopy(self.state).push(move)), pActs[moveIndex])
+
+    #Checks to see if the node is terminal.
+    def has_children(self):
+        return len(self.children) > 0
+
+    #Uses PUCT to find the Best Action.
+    def bestAction(self):
+        puctList = []
+
+        #Set c to  1 (can be tuned later).
+        c = 1
+
+        #Calculates the N sum.
+        NList = [self.children[act].N for act in self.children.keys()]
+        
+        NSum = sum(NList)
+        NSum  = np.sqrt(NSum)
+
+        for acts in self.children.keys():
+            edge = self.children[acts]
+
+            U = c * edge.P * NSum / (1+edge.N)
+            Q = edge.Q
+
+            puctList.append(Q+U)
+
+        bestAct = np.argmax(puctList)
+
+        bestEdge = self.children[bestAct]
+
+        return bestEdge.nextState
+
+
+
+
+    #Picks a random child to expand and expands it.
+    #def select_random(self):
+
+class Action:
+    def __init__(self, child, P):
+        self.P = P
+        self.N = 0
+        self.W = 0
+        self.Q = 0
+
+        self.nextState = child
+
+
+    
+
---
+++ mcts.py
@@ -0,0 +1,69 @@
+import ChessGame as cg
+import mcNode as mc
+
+from collections import defaultdict
+import numpy as np 
+import torch as th
+
+import math
+import queue
+import random
+import copy
+import numpy as np
+import itertools
+
+
+class MCTS:
+
+    #Initializes by starting a game from the beginning.
+    def __init__(self, board):
+        #Intializes a root with empty parents.
+        self.game = board
+        self.root = mc.MCNode(self.game.get_board())
+
+    # Picks a move from the current tree
+    def select(self, tau = 1, explore = True):
+        idxs = self.root.children.keys()
+        ns = np.array([self.root.children[i].N for i in idxs])
+        ns = ns / np.sum(ns)
+
+        if explore:
+            ns = ns**(1 / tau)
+            nextIdx = random.choices(idxs, ns)
+        else:
+            nextIdx = np.argmax(ns)
+
+        # nextIdx should be an int from 0 to 64 x 73 - 1.
+        # Use utils.idxToMove() to convert to a move.
+        return nextIdx, self.root.pActs
+
+
+    # Expand down into the tree recursively and find a leaf node.
+    # When we find the leaf node, query the NN to initialize its children.
+    def search(self, node, net):
+
+        #Once we reach the leaf node, return the NN's assesment of the current state.
+        if not node.has_children():
+            p, v = net(node.state)
+
+            node.createChildren(p)
+            return -v
+
+        a = node.bestAction()
+        p, v = self.search(a)
+
+        # Backpropagation step
+        a.N += 1
+        a.W += v
+        a.Q = a.N / a.W
+
+        return -v
+
+        
+
+
+
+
+
+
+    
