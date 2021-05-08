3206cc675c2c68e104f4e83276f2e7064bd1b8d1
Enrikina <enrikina@seas.upenn.edu>
2021-05-08 00:00:24 -0400

    Added cuda code to main loop.

--- src/mainEpochsEpoch.py
+++ src/mainEpochsEpoch.py
@@ -3,6 +3,7 @@ from alphaZeroNet import ChessNet, train
 import chessArena
 import torch
 import copy
+from chessArena import ChessArena
 
 
 fromScratch = True # flag if running with no saved NN
@@ -13,10 +14,12 @@ if fromScratch:
 else:
     player1 = ChessNet()
     player1.load_state_dict(torch.load("bestplayer.pth"))
-    
+
+
     
 player2 = copy.deepcopy(player1)
 player2.eval()
+
 
 numNewPlayers = 10 # end the updates after 10 new better versions have been released
 
@@ -24,11 +27,14 @@ numNewPlayers = 10 # end the updates after 10 new better versions have been rele
 iters = 0
 while iters < numNewPlayers:
     # generate data
-    genData(player1, num_games = 1000, saveFile = True)
+    genData(player1, num_games = 50, saveFile = True)
     dataset = loadData()
     
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.to(device)    
+    
     train(player1, player1)
-    replaced, winner, games = chessArena(player2, player1)
+    replaced, winner, games = ChessArena(player2, player1)
     if replaced:
         player1 = copy.deepcopy(winner)
         player2 = copy.deepcopy(winner)
