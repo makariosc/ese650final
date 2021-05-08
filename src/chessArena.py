98499a508ce0a9f707024108ae3d086b46908e92
Makarios Chung <chungma@seas.upenn.edu>
2021-05-08 02:31:22 -0400

    chessArena mp

--- src/chessArena.py
+++ src/chessArena.py
@@ -2,7 +2,38 @@ import numpy as np
 import ChessGame
 import torch
 import chess
+import random
 from datetime import datetime
+
+import multiprocessing as mp
+
+def arenaWorker(nns):
+
+    old, new = nns
+    oldResult = 0
+    newResult = 0
+
+    coinFlip = random.random()
+
+    if coinFlip < 0.5:
+        # old is white, new is black
+        cg = ChessGame.ChessGame(old, new)
+        moves = cg.gameLoop()
+
+        oldResult = moves[chess.WHITE][0][2]
+        newResult = -oldResult
+    else:
+        cg = ChessGame.ChessGame(new, old)
+        moves = cg.gameLoop()
+
+        oldResult = moves[chess.BLACK][0][2]
+        newResult = -oldResult
+
+    ds = []
+    ds += moves[0]
+    ds += moves[1]
+
+    return (oldResult, newResult), ds
 
 def ChessArena(oldNN, newNN, numGames = 50):
 
@@ -11,25 +42,34 @@ def ChessArena(oldNN, newNN, numGames = 50):
 
     tSet = []
 
-    for i in range(numGames):
-        if i % 2 == 0:
-            cg = ChessGame.ChessGame(oldNN, newNN)
-            moves = cg.gameLoop()
-            tSet.append(moves)
+    pool = mp.Pool()
+    outcomes = pool.imap(arenaWorker, [(oldNN, newNN)] * numGames, 4)
 
-            if moves[chess.WHITE][0][2] == 1:
-                numOldWins += 1
-            elif moves[chess.WHITE][0][2] == -1:
-                numNewWins += 1
-        else:
-            cg = ChessGame.ChessGame(newNN, oldNN)
-            moves = cg.gameLoop()
-            tSet.append(moves)
+    for o in outcomes:
+        numOldWins += o[0][0]
+        numNewWins += o[0][1]
+        for datapoint in o[1]:
+            tSet.append(datapoint)
 
-            if moves[chess.WHITE][0][2] == 1:
-                numNewWins += 1
-            elif moves[chess.WHITE][0][2] == -1:
-                numOldWins += 1
+    # for i in range(numGames):
+    #     if i % 2 == 0:
+    #         cg = ChessGame.ChessGame(oldNN, newNN)
+    #         moves = cg.gameLoop()
+    #         tSet.append(moves)
+    #
+    #         if moves[chess.WHITE][0][2] == 1:
+    #             numOldWins += 1
+    #         elif moves[chess.WHITE][0][2] == -1:
+    #             numNewWins += 1
+    #     else:
+    #         cg = ChessGame.ChessGame(newNN, oldNN)
+    #         moves = cg.gameLoop()
+    #         tSet.append(moves)
+    #
+    #         if moves[chess.WHITE][0][2] == 1:
+    #             numNewWins += 1
+    #         elif moves[chess.WHITE][0][2] == -1:
+    #             numOldWins += 1
 
     #Return condition. Returns the new NN if it won over 55% of its games.
     # Otherwise returns the old one.
