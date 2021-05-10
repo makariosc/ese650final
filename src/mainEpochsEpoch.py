2b6532de8c22a0b382777825244e6631075ffc14
Makarios Chung <chungma@seas.upenn.edu>
2021-05-10 02:02:13 -0400

    wat

--- src/mainEpochsEpoch.py
+++ src/mainEpochsEpoch.py
@@ -3,38 +3,41 @@ import torch
 import copy
 import Arena
 
-fromScratch = True # flag if running with no saved NN
 
-if fromScratch:
-    # initialize some new network
-    player1 = OthelloNNet()
-else:
-    player1 = OthelloNNet()
-    player1.load_state_dict(torch.load("bestplayer.pt"))
+if __name__ == "__main__":
 
-player2 = copy.deepcopy(player1)
+    fromScratch = True # flag if running with no saved NN
 
-player1.eval()
-player2.eval()
+    if fromScratch:
+        # initialize some new network
+        player1 = OthelloNNet()
+    else:
+        player1 = OthelloNNet()
+        player1.load_state_dict(torch.load("bestplayer.pt"))
 
-numNewPlayers = 10 # end the updates after 10 new better versions have been released
+    player2 = OthelloNNet()
 
-# player1 will be the best current player that we try to train
-iters = 0
-while iters < numNewPlayers:
-    # generate data
-    _, dataset = Arena.Arena(player1, player1)
+    player1.eval()
+    player2.eval()
 
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    player1.to(device)
+    numNewPlayers = 10 # end the updates after 10 new better versions have been released
 
-    train(player1, dataset)
+    # player1 will be the best current player that we try to train
+    iters = 0
+    while iters < numNewPlayers:
+        # generate data
+        _, dataset = Arena.Arena(player1, player1)
 
-    player1.to('cpu')
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        player1.to(device)
 
-    replaced, winner, games = Arena.Arena(player2, player1, 8)
-    if replaced:
-        player1 = copy.deepcopy(winner)
-        player2 = copy.deepcopy(winner)
-        player2.eval()
-        iters += 1
+        train(player1, dataset)
+
+        player1.to('cpu')
+
+        replaced, winner, games = Arena.Arena(player2, player1, 8)
+        if replaced:
+            player1 = copy.deepcopy(winner)
+            player2 = copy.deepcopy(winner)
+            player2.eval()
+            iters += 1
